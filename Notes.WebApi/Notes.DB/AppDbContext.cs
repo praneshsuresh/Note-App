3ced5c78722501adb0e0b3dffd2c1b8eedd98a7e
Pranesh Raj Suresh <57155287+praneshsuresh@users.noreply.github.com>
2021-02-14 18:19:57 -0600

    Core and Db

---
+++ Notes.WebApi/Notes.DB/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notes.DB
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Note> Notes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=Notes;Integrated Security=True");
+        }
+
+    }
+}
