d79f2203016bb2c71b4ac21975d194c98bfe9ffe
Pranesh Raj Suresh <57155287+praneshsuresh@users.noreply.github.com>
2021-02-14 18:25:10 -0600

    Delete src directory

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import NotesReducer from './notesReducer';
-
-export default configureStore({
-  reducer: {
-    notesReducer: NotesReducer,
-  },
-});
